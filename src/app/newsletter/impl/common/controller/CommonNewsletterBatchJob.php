943caf803cbf1df5c847cad6d748d0eafe2e9acd
Thomas Guenther <guenther@hnm.ch>
2023-02-16 11:50:39 +0100

    HistoryEntryGenerator in BatchJob

--- src/app/newsletter/impl/common/controller/CommonNewsletterBatchJob.php
+++ src/app/newsletter/impl/common/controller/CommonNewsletterBatchJob.php
@@ -6,22 +6,27 @@ use n2n\core\container\TransactionManager;
 use newsletter\impl\common\model\CommonNewsletterConfig;
 use newsletter\core\model\SendBatchDao;
 use newsletter\impl\common\model\CommonNewsletterSetuper;
+use newsletter\core\model\HistoryEntryGenerator;
 
-class SendBatchController implements RequestScoped {
+class CommonNewsletterBatchJob implements RequestScoped {
 
 	private $sendBatchDao;
 	private $config;
+	private $historyEntryGenerator;
 
-	private function _init(SendBatchDao $sendBatchDao, CommonNewsletterConfig $config, CommonNewsletterSetuper $setuper) {
+	private function _init(HistoryEntryGenerator $historyEntryGenerator, SendBatchDao $sendBatchDao, 
+			CommonNewsletterConfig $config, CommonNewsletterSetuper $setuper) {
 		$this->config = $config;
 		$this->sendBatchDao = $sendBatchDao;
+		$this->historyEntryGenerator = $historyEntryGenerator;
 		$setuper->setup();
 	}
 	
 	public function _onTrigger(TransactionManager $tm) {
 		// can also run in dev mode, as NewsletterMail returns system manager mail on dev mode
 		// if (N2N::isDevelopmentModeOn()) return;
-		
+		set_time_limit(0);
+		$this->historyEntryGenerator->buildHistoryEntriesForFirstUnpreparedHistory();
 		$this->sendBatchDao->sendMails($this->config->isCronjobAvailable(), $this->config->getNumMailsPerHour(), 
 				$this->config->getNumMailsPerRequest());
 	}
